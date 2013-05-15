f29797279a5d57c9e209b3c2fd0b925ef8594d8f
agusti <agusti.dosaiguas@takkmama.com>
2013-05-15 22:59:38 +0200

    Added sayBye method

--- test/hello.test.js
+++ test/hello.test.js
@@ -4,10 +4,22 @@ exports.testSayHello = function(test) {
 	test.expect(2);
 
 	var world = new Hello();
-	test.equal(world.sayHello(), "Hello World!");
-
 	var maika = new Hello("Maika!");
+
+	test.equal(world.sayHello(), "Hello World!");
 	test.equal(maika.sayHello(), "Hello Maika!");
+
+	test.done();
+};
+
+exports.testSayBye = function(test) {
+	test.expect(2);
+
+	var world = new Hello();
+	var maika = new Hello("Maika!");
+
+	test.equal(world.sayBye(), "Goodbye World!");
+	test.equal(maika.sayBye(), "Goodbye Maika!");
 
 	test.done();
 };
